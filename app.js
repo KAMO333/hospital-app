bce803947dceb3b71da16d729e625b67ef9c01ec
Moroka <mmopanemoroka@gmail.com>
2022-12-24 02:53:38 +0000

    added initial code

---
+++ app.js
@@ -0,0 +1,49 @@
+// class App {
+//   constructor() {
+//     this.form = {};
+
+//     this.$app = document.querySelector("#app");
+//     this.$firebaseAuthContainer = document.querySelector(
+//       "#firebaseui-auth-container"
+//     );
+//     this.$logout = document.querySelector(".logout");
+
+//     this.ui = new firebaseui.auth.AuthUI(auth);
+//     this.handleAuth();
+
+//     this.addEventListener();
+//   }
+
+//   handleAuth() {
+//     firebase.auth().onAuthStateChanged((user) => {
+//       if (user) {
+//         this.userId = user.uid;
+//         this.redirectToApp();
+//       } else {
+//         this.redirectToAuth();
+//       }
+//     });
+//   }
+//   redirectToApp() {
+//     this.$firebaseAuthContainer.style.display = "none";
+//     this.$app.style.display = "block";
+//   }
+//   redirectToAuth() {
+//     this.$firebaseAuthContainer.style.display = "block";
+//     this.$app.style.display = "none";
+//     this.ui.start("#firebaseui-auth-container", {
+//       signInOptions: [
+//         firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//       ],
+//       // Other config options...
+//     });
+//   }
+
+//   addEventListener() {
+//     this.$logout.addEventListener("click", () => {
+//       console.log("CLICKED");
+//     });
+//   }
+// }
+// const app = new App();
